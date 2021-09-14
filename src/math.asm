bc51351b7cd8f3c49133a52cec1905ded003e3dc
Ivan Nikolaev <voidexp@gmail.com>
2021-09-14 17:56:10 +0300

    Codestyle

--- src/math.asm
+++ src/math.asm
@@ -21,17 +21,17 @@
 ;
 .proc check_rect_intersection
             lda var7
-            cmp var1        ; R2 < L1? First's left edge is to the right of second's right edge?
+            cmp var1                ; R2 < L1? First's left edge is to the right of second's right edge?
             bcc @nooverlap
             lda var3
-            cmp var5        ; R1 < L2? First's right edge is to the left of second's left edge?
+            cmp var5                ; R1 < L2? First's right edge is to the left of second's left edge?
             bcc @nooverlap
             lda var8
-            cmp var2        ; B2 < T1? First's top edge is below second's bottom edge?
+            cmp var2                ; B2 < T1? First's top edge is below second's bottom edge?
             bcc @nooverlap
             lda var4
-            cmp var6        ; B1 < T2? First's bottom edge is above second's top edge?
+            cmp var6                ; B1 < T2? First's bottom edge is above second's top edge?
             bcc @nooverlap
-            sec             ; the rectangles intersect, set carry flag
-@nooverlap: rts             ; carry propagated from instructions above
+            sec                     ; the rectangles intersect, set carry flag
+@nooverlap: rts                     ; carry propagated from instructions above
 .endproc
